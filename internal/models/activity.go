f990d147327b05fcd80352777a5b4504216d2d38
rayprastya <rayhan.prastya@gmail.com>
2025-09-04 06:26:18 +0700

    feat: add form struct for entitiy

--- internal/models/activity.go
+++ internal/models/activity.go
@@ -3,7 +3,7 @@ package models
 type Activity struct {
 	BaseEntity
 
-	ActivityType      string `json:"activity_type"`
-	DurationInMinutes int    `json:"duration_in_minutes"`
-	CaloriesBurned    int    `json:"calories_burned"`
+	ActivityType      string `gorm:"column:activity_type;type:varchar(100);not null" json:"activity_type"`
+	DurationInMinutes int    `gorm:"column:duration_in_minutes;not null" json:"duration_in_minutes"`
+	CaloriesBurned    int    `gorm:"column:calories_burned;not null" json:"calories_burned"`
 }
