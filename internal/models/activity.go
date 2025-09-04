50a65884944dd042aff31768219b5594a3aee69f
rayprastya <rayhan.prastya@gmail.com>
2025-09-05 05:40:26 +0700

    refactor: match the structure with the assignment

--- internal/models/activity.go
+++ internal/models/activity.go
@@ -1,11 +1,14 @@
 package models
 
+import "time"
+
 type Activity struct {
 	BaseEntity
 
-	ActivityType      string `gorm:"column:activity_type;type:varchar(100);not null" json:"activity_type"`
-	DurationInMinutes int    `gorm:"column:duration_in_minutes;not null" json:"duration_in_minutes"`
-	CaloriesBurned    int    `gorm:"column:calories_burned;not null" json:"calories_burned"`
+	ActivityType      string    `gorm:"column:activity_type;type:varchar(100);not null" json:"activity_type"`
+	DurationInMinutes int       `gorm:"column:duration_in_minutes;not null" json:"duration_in_minutes"`
+	CaloriesBurned    int       `gorm:"column:calories_burned;not null" json:"calories_burned"`
+	DoneAt            time.Time `gorm:"column:done_at;not null" json:"done_at"`
 }
 
 type UpdateActivityRequest struct {
