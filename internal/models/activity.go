8099871e4889a86132d9392ba9fb9215c90a7e02
Mona-kecil <flowyourminds@gmail.com>
2025-09-04 13:27:07 +0700

    Add update activity request type

--- internal/models/activity.go
+++ internal/models/activity.go
@@ -7,3 +7,9 @@ type Activity struct {
 	DurationInMinutes int    `gorm:"column:duration_in_minutes;not null" json:"duration_in_minutes"`
 	CaloriesBurned    int    `gorm:"column:calories_burned;not null" json:"calories_burned"`
 }
+
+type UpdateActivityRequest struct {
+	ActivityType      *string `gorm:"column:activity_type;type:varchar(100);not null" json:"activity_type"`
+	DurationInMinutes *int    `gorm:"column:duration_in_minutes;not null" json:"duration_in_minutes"`
+	CaloriesBurned    *int    `gorm:"column:calories_burned;not null" json:"calories_burned"`
+}
