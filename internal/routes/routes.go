ae4299eb41722b19d4455e13f89dbf3b91ba7ead
Nurfadillah Umar <itsennuu@gmail.com>
2025-09-05 00:52:09 +0800

    feat: add  login route

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -7,12 +7,12 @@ import (
 )
 
 // SetupRoutes configures all the routes for the application
-func SetupRoutes(router *gin.Engine, healthHandler *handlers.HealthHandler, userHandler *handlers.UserHandler, registerHandler *handlers.RegisterHandler) {
+func SetupRoutes(router *gin.Engine, healthHandler *handlers.HealthHandler, userHandler *handlers.UserHandler, registerHandler *handlers.RegisterHandler, loginHandler *handlers.LoginHandler) {
 	// API version 1
 	v1 := router.Group("/api/v1")
 	{
 		// Login & register routes
-		// v1.POST("/login", loginHandler.Login)
+		v1.POST("/login", loginHandler.Login)
 		v1.POST("/register", registerHandler.Register)
 
 		// Health check routes
