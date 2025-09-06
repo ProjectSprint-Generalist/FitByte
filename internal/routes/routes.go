41e8ebbcf13ff7d3517717055b481ad3634ddac3
adinfahru <mohamad.fahrudin18@gmail.com>
2025-09-06 23:04:52 +0700

    fix: update API versioning path in route setup

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -10,7 +10,7 @@ import (
 // SetupRoutes configures all the routes for the application
 func SetupRoutes(router *gin.Engine, healthHandler *handlers.HealthHandler, userHandler *handlers.UserHandler, registerHandler *handlers.RegisterHandler, loginHandler *handlers.LoginHandler, activityHandler *handlers.ActivityHandler) {
 	// API version 1
-	v1 := router.Group("/api/v1")
+	v1 := router.Group("/v1")
 	{
 		// Login & register routes
 		v1.POST("/login", loginHandler.Login)
